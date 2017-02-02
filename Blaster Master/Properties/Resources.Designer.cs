6e6161964f28a5fb9849fc0e2bd9667bf0727864
subhayonB <binkubix@gmail.com>
2017-02-02 10:43:28 -0700

    new bullet

--- Blaster Master/Properties/Resources.Designer.cs
+++ Blaster Master/Properties/Resources.Designer.cs
@@ -313,6 +313,16 @@ namespace BlasterMaster.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap mBullets {
+            get {
+                object obj = ResourceManager.GetObject("mBullets", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap pickupA {
             get {
                 object obj = ResourceManager.GetObject("pickupA", resourceCulture);
